25e7107135c8ef3fd883b9145149d8253ea90a88
masterofray <aryanto.dandan@gmail.com>
2025-10-10 13:51:21 +0900

    finalizd the notebook and writing the reports paper

---
+++ reports/Planning.mm
@@ -0,0 +1,99 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Stunting Paper" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1758545396307" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false" show_icons="BESIDE_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Title" POSITION="bottom_or_right" ID="ID_340076297" CREATED="1758504309794" MODIFIED="1758545396292" HGAP_QUANTITY="-218.49999 pt" VSHIFT_QUANTITY="242.25 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Pemodelan biner terkait stunting yang terjadi di kabupaten Mamberamo menggunakan XGBoost" ID="ID_877750666" CREATED="1758504505372" MODIFIED="1758504622072" HGAP_QUANTITY="-328.74999 pt" VSHIFT_QUANTITY="-8.25 pt" TEXT_ALIGN="RIGHT"/>
+<node TEXT="Penerapan model XGBoost untuk data stunting kabupaten Mamberamo Raya Papua" ID="ID_446077239" CREATED="1758504514954" MODIFIED="1758504623750" HGAP_QUANTITY="-320.49999 pt" VSHIFT_QUANTITY="6.75 pt" TEXT_ALIGN="RIGHT"/>
+<node TEXT="Optimalisasi model XGBoost untuk memprediksi kasus Stunting yang terjadi di Kabupatn Mamberamo Raya" ID="ID_803344154" CREATED="1758545334721" MODIFIED="1758545387155" HGAP_QUANTITY="-315.24999 pt" VSHIFT_QUANTITY="16.5 pt" TEXT_ALIGN="RIGHT"/>
+</node>
+<node TEXT="Abstract" POSITION="bottom_or_right" ID="ID_43648955" CREATED="1758504324668" MODIFIED="1758504401029" VSHIFT_QUANTITY="-57 pt">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Analysis" POSITION="bottom_or_right" ID="ID_721998675" CREATED="1758504348692" MODIFIED="1758504462688" HGAP_QUANTITY="-252.24999 pt" VSHIFT_QUANTITY="89.25 pt">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Result" POSITION="bottom_or_right" ID="ID_1260216985" CREATED="1758504366060" MODIFIED="1758504454500" HGAP_QUANTITY="75.5 pt" VSHIFT_QUANTITY="140.25 pt">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Reference" POSITION="bottom_or_right" ID="ID_1641679749" CREATED="1758504374964" MODIFIED="1758504439158" HGAP_QUANTITY="-22 pt" VSHIFT_QUANTITY="156.75 pt">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Planning" POSITION="bottom_or_right" ID="ID_920105081" CREATED="1758504429932" MODIFIED="1758545413187" HGAP_QUANTITY="-261.99999 pt" VSHIFT_QUANTITY="-341.99999 pt">
+<edge COLOR="#00007c"/>
+</node>
+</node>
+</map>
